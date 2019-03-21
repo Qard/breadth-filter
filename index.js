48842535acb077f0421ab5283bbb2b6b31929590
Stephen Belanger <admin@stephenbelanger.com>
2019-03-21 09:34:48 +0100

    Externalize circularity handling (#9)

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const entries = require('object.entries')
 
-function targetFor (source, destructive) {
+function defaultOnArray () { return [] }
+function defaultOnObject () { return {} }
+
+function targetFor (source, key, fieldPath, isNew, {
+  onArray = defaultOnArray,
+  onObject = defaultOnObject
+} = {}) {
   if (Array.isArray(source)) {
-    return destructive ? source : []
+    return onArray(source, key, fieldPath, isNew)
   } else if (source !== null && typeof source === 'object') {
-    return destructive ? source : {}
+    return onObject(source, key, fieldPath, isNew)
   }
 }
 
-module.exports = function breadthFilter (root, fn, destructive) {
-  const target = targetFor(root, destructive)
+module.exports = function breadthFilter (root, opts = {}) {
+  const { onValue } = opts
+  const target = targetFor(root, null, [], true, opts)
   if (!target) return root
 
   const queue = [[ root, target, [] ]]
-  const seen = new Set()
+  const seen = new Set([ root ])
   let item
 
   while (item = queue.shift()) {
     const [ source, target, path ] = item
     for (const [ key, value ] of entries(source)) {
       const fieldPath = path.concat(key)
-      const newTarget = targetFor(value, destructive)
+      const isNew = !seen.has(value)
+      if (isNew) seen.add(value)
+
+      const newTarget = targetFor(value, key, fieldPath, isNew, opts)
       if (newTarget) {
-        if (!seen.has(value)) {
-          seen.add(value)
-        } else {
-          continue
-        }
         target[key] = newTarget
-        queue.push([ value, target[key], fieldPath ])
+        if (isNew) {
+          queue.push([ value, target[key], fieldPath ])
+        }
       } else {
-        target[key] = fn(value, key, fieldPath)
+        target[key] = onValue(value, key, fieldPath)
       }
     }
   }
